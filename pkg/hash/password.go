062153c53a3d65647bb61e4dc7e4ce521d4d9218
Yaroslav Yarosh <yaroslavyarosh@protonmail.com>
2022-06-05 15:14:03 +0300

    -

--- pkg/hash/password.go
+++ pkg/hash/password.go
@@ -20,7 +20,7 @@ func NewPasswordManager(salt string) *passwordManager {
 }
 
 func (p *passwordManager) Hash(password string) string {
-	hash := sha256.Sum256([]byte(password))
+	hash := sha256.Sum256([]byte(password + p.salt))
 
 	return fmt.Sprintf("%x", hash)
 }
