de111dc1620e7ab5cff932ff3bed7bc424e76c51
Yaroslav Yarosh <yaroslavyarosh@protonmail.com>
2022-06-09 22:57:00 +0300

    -

--- internal/entity/note.go
+++ internal/entity/note.go
@@ -4,6 +4,7 @@ type Note struct {
 	Id         string   `json:"id" gorm:"primaryKey"`
 	Title      string   `json:"title"`
 	Text       string   `json:"text"`
+	IsBookmark bool     `json:"is_bookmark" gorm:"default:false"`
 	NotebookId string   `json:"notebook_id"`
 	UserId     string   `json:"user_id"`
 	Notebook   Notebook `gorm:"foreignKey:notebook_id"`
